4dbbed104cb46eb70622cfd4d39b75a224d48603
Daniel Cabral <13870847+daniel3385@users.noreply.github.com>
2025-12-25 16:55:40 -0300

    Example of pooling using switch0

---
+++ 02-button-pooling/src/main.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   100
+
+/* The devicetree node identifier for the "led0" alias and "switch0". */
+#define LED0_NODE DT_ALIAS(led0)
+#define SW0_NODE	DT_ALIAS(sw0) 
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+int main(void)
+{
+	int ret;
+	bool led_state = true;
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	if (!device_is_ready(button.port)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (1) {
+		bool val = gpio_pin_get_dt(&button);
+		gpio_pin_set_dt(&led,val);
+		k_msleep(SLEEP_TIME_MS);
+	}
+	return 0;
+}
