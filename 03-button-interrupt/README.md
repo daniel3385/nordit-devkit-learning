30c2e058b239ba5108439fb625a8313f1a341503
Daniel Cabral <13870847+daniel3385@users.noreply.github.com>
2025-12-25 17:20:55 -0300

    Add project 03 which uses swtich 0 isr to toggles led

---
+++ 03-button-interrupt/README.md
@@ -0,0 +1,18 @@
+# Button and LED Example (Switch 0) using interrupt
+
+This project is a simple application that **reads the button Switch 0 using GPIO interruption** and **toggles the LED when the button is pressed**.
+
+The button is configured as a GPIO input with an **interrupt handler**, allowing the system to detect button presses efficiently without continuous polling.  
+When Switch 0 is pressed, an interrupt is triggered and the LED turns on. When the button is released, the LED turns off.
+
+This project was created for learning and testing purposes.
+
+## Features
+- Button input using GPIO interrupt
+- Controls LED using GPIO interrupt callback
+- LED toggles when button is pressed
+
+## Hardware
+- Nordic Semiconductor development board
+- Switch 0
+- On-board LED
---
+++ 03-button-interrupt/src/main.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+/* 10 minutes in ms */
+#define SLEEP_TIME_MS   10*60*1000
+
+/* The devicetree node identifier for the "led0" alias and "switch0". */
+#define LED0_NODE   DT_ALIAS(led0)
+#define SW0_NODE	DT_ALIAS(sw0) 
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led 	= GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+/*
+ * Callback variable and function
+ */
+static struct gpio_callback button_cb_data;
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
+
+int main(void)
+{
+	int ret;
+	bool led_state = true;
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	if (!device_is_ready(button.port)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+
+	while (1) {
+		k_msleep(SLEEP_TIME_MS);
+	}
+
+	return 0;
+}
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    gpio_pin_toggle_dt(&led);
+}
